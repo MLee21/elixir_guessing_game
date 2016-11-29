f716c4b88293805d4f08b0529903e70d5c0b6cd1
MLee21 <minnie.lee89@gmail.com>
2016-11-28 21:39:47 -0600

    wrote some tests. woot woot

--- guessing/test/guessing_test.exs
+++ guessing/test/guessing_test.exs
@@ -2,7 +2,15 @@ defmodule GuessingTest do
   use ExUnit.Case
   doctest Guessing
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "Checking valid user input" do
+    assert Guessing.check_user_input(5) == {:ok, 5}
+    assert Guessing.check_user_input(9) == {:ok, 9}
+    assert Guessing.check_user_input(10) == {:ok, 10}
+  end
+
+  test "Invalid user input" do
+    # Check user input bigger than 10
+    assert Guessing.check_user_input(11) == {:error, "11 is not a valid number. Please input a number between 1-10"}
+    assert Guessing.check_user_input(22) == {:error, "22 is not a valid number. Please input a number between 1-10"}
   end
 end
