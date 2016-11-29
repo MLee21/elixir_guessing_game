0a1d9a37dc18eb21896f6a4e97e31f1ff82369d2
MLee21 <minnie.lee89@gmail.com>
2016-11-28 21:46:49 -0600

    wrote recursive methods to check whether a number is between 1-10 and to spit out an error message if the number is outside of that range. green tests.

--- guessing/lib/guessing.ex
+++ guessing/lib/guessing.ex
@@ -1,2 +1,10 @@
 defmodule Guessing do
+
+  def check_user_input(number) when number <= 10 do
+    {:ok, number}
+  end
+
+  def check_user_input(number) do
+    {:error, "#{number} is not a valid number. Please input a number between 1-10"}
+  end
 end
